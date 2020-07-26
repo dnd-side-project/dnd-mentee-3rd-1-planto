4e11f9423bbc2c4c31487dc917130be04cc69fc4
Aaron Lab <aaronlab.net@gmail.com>
2020-07-26 21:19:31 +0900

    ios: first/second intro VC image

--- front-end/iOS/Planto/Planto/Intro/FirstIntroViewController.swift
+++ front-end/iOS/Planto/Planto/Intro/FirstIntroViewController.swift
@@ -1,24 +1,19 @@
 //
-//  ViewController.swift
-//  Plento
+//  FirstIntroViewController.swift
+//  Planto
 //
-//  Created by Planto on 7/19/20.
+//  Created by Planto on 7/26/20.
 //  Copyright © 2020 Planto. All rights reserved.
 //
 
 import UIKit
 
 class FirstIntroViewController: UIViewController {
-    
-    let delayTime: Double = 3   // Delay Time 변수
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // delayTime 초 후 다음 뷰 전환
-        DispatchQueue.main.asyncAfter(deadline: .now() + delayTime, execute: {
-            self.performSegue(withIdentifier: "SecondIntro", sender: self)
-        })
-    }
-}
 
+        // Do any additional setup after loading the view.
+    }
+
+}
