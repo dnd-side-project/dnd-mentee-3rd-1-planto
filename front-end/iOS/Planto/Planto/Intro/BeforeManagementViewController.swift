fe09e060f22f2134ad7c7bea2b66904b32ba56fc
김호준 <elddy0948@hotmail.co.kr>
2020-07-28 02:20:55 +0900

    iOS: Add BeforeManagementVC , 오류 수정

---
+++ front-end/iOS/Planto/Planto/Intro/BeforeManagementViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BeforeManagementViewController.swift
+//  Planto
+//
+//  Created by 김호준 on 2020/07/28.
+//  Copyright © 2020 D&D. All rights reserved.
+//
+
+import UIKit
+
+class BeforeManagementViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
