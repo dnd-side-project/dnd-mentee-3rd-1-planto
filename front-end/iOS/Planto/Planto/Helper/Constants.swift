8e60566bd57373f7a356cb3809d66e49271eb740
김호준 <elddy0948@hotmail.co.kr>
2020-07-27 21:41:18 +0900

    iOS: pull

---
+++ front-end/iOS/Planto/Planto/Helper/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Planto
+//
+//  Created by 김호준 on 2020/07/27.
+//  Copyright © 2020 D&D. All rights reserved.
+//
+
+import Foundation
---
+++ front-end/iOS/Planto/Planto/Intro/ConfirmNameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ConfirmNameViewController.swift
+//  Planto
+//
+//  Created by 김호준 on 2020/07/27.
+//  Copyright © 2020 D&D. All rights reserved.
+//
+
+import UIKit
+
+class ConfirmNameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
