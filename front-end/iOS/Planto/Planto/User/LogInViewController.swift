628a2e12c1df67bad1aaeda90d524d3a0587343c
Aaron Lab <aaronlab.net@gmail.com>
2020-07-31 17:02:35 +0900

    ios: Auto Login Toggle

--- front-end/iOS/Planto/Planto/User/LogInViewController.swift
+++ front-end/iOS/Planto/Planto/User/LogInViewController.swift
@@ -9,12 +9,47 @@
 import UIKit
 
 class LogInViewController: UIViewController {
-
+    
+    // MARK: - Mutable Variables
+    var isAutoLogin: Bool = false
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var btnAutoLogin: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        btnAutoLogin.isSelected = false
     }
     
 
+}
+
+// MARK: - IBActions
+extension LogInViewController {
+    // Auto Login 체크 박스
+    @IBAction func toggleAutoLogin(_ sender: UIButton) {
+        autoLoginToggle()
+    }
+    
+    // Auto Login 텍스트 버튼
+    @IBAction func toggleAutoLoginText(_ sender: UIButton) {
+        autoLoginToggle()
+    }
+}
+
+// MARK: - Functions
+extension LogInViewController {
+    // Auto Login 여부 확인
+    func autoLoginToggle() {
+        btnAutoLogin.isSelected = !btnAutoLogin.isSelected
+        if (btnAutoLogin.isSelected == true) {
+            isAutoLogin = true
+        } else {
+            isAutoLogin = false
+        }
+        print(isAutoLogin)
+    }
 }
