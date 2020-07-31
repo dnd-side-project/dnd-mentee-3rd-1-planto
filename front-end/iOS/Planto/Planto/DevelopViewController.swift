b888395cceb41049a4e5224e449ad88146531981
Aaron Lab <aaronlab.net@gmail.com>
2020-07-31 16:12:50 +0900

    ios: LogInVC Boilerplate

---
+++ front-end/iOS/Planto/Planto/DevelopViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DevelopViewController.swift
+//  Planto
+//
+//  Created by Aaron Lee on 7/31/20.
+//  Copyright © 2020 D&D. All rights reserved.
+//
+
+import UIKit
+
+class DevelopViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
