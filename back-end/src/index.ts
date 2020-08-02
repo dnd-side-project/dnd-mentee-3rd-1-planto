80745610917f8ea3427a0ee8f5564cb30916de32
Dong-Young Kim <31337.persona@gmail.com>
2020-08-02 19:12:33 +0900

    refactor(user): remove user stub

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,38 +1,15 @@
 import * as express from "express";
 import { Request, Response } from "express";
 import { createConnection } from "typeorm";
-import { User } from "./entity/User";
 
 createConnection()
   .then((connection) => {
-    const userRepository = connection.getRepository(User);
-
     // create express app
     const app = express();
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
 
     // register express routes
-    app.get("/users", async (req: Request, res: Response) => {
-      const users = await userRepository.find();
-      res.json(users);
-    });
-
-    app.get("/users/:id", async (req: Request, res: Response) => {
-      const user = await userRepository.findOne(req.params.id);
-      res.send(user);
-    });
-
-    app.post("/users", async (req: Request, res: Response) => {
-      const user = await userRepository.create(req.body);
-      const result = await userRepository.save(user);
-      res.send(result);
-    });
-
-    app.delete("/users/:id", async (req: Request, res: Response) => {
-      const result = await userRepository.delete(req.params.id);
-      res.send(result);
-    });
 
     // setup express app here
     // ...
