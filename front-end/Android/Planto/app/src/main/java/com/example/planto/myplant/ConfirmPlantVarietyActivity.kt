dcac6157c0ab576986ab8a1a9934ecd9d3d76bd8
DESKTOP-I49D4M0\agohd <agohdasol@gmail.com>
2020-08-07 10:48:34 +0900

    Android : Add Plant View - 식물 품종 검색 뷰, 검색 결과 뷰, 식물 이름 입력 뷰 추가

---
+++ front-end/Android/Planto/app/src/main/java/com/example/planto/myplant/ConfirmPlantVarietyActivity.kt
@@ -0,0 +1,29 @@
+package com.example.planto.myplant
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.planto.R
+import kotlinx.android.synthetic.main.activity_confirm_plant_variety.*
+
+class ConfirmPlantVarietyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirm_plant_variety)
+
+        // 이미지 스케일 조절
+        confirmPlantVarietyImageView.scaleX = 0.95f
+        confirmPlantVarietyImageView.scaleY = 0.95f
+
+        // 확인 버튼 클릭
+        buttonConfirmPlantVarietyConfirm.setOnClickListener {
+            // To Do: next view(GetPlantNameActivity) 이동
+        }
+
+        // 취소 버튼 클릭
+        buttonConfirmPlantVarietyCancel.setOnClickListener {
+            val intent = Intent(baseContext, SearchPlantVarietyActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ front-end/Android/Planto/app/src/main/java/com/example/planto/myplant/SearchPlantVarietyActivity.kt
@@ -0,0 +1,33 @@
+package com.example.planto.myplant
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.planto.R
+import kotlinx.android.synthetic.main.activity_search_plant_variety.*
+
+class SearchPlantVarietyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_plant_variety)
+        // 이미지 왼쪽으로 15도 회전
+        searchPlantVarietyImageView.rotation = -15f
+
+        // 이미지 스케일 조절
+        searchPlantVarietyImageView.scaleX = 0.95f
+        searchPlantVarietyImageView.scaleY = 0.95f
+
+        // 검색 버튼 클릭
+        buttonSearchPlantVarietyConfirm.setOnClickListener {
+            // ToDo: DB로부터 식물 품종 데이터 검색하여 식물 품종명이 있으면 해당 품종이름을, 없으면 커스텀 품종이름을 지정하도록 할것
+            val intent = Intent(baseContext, ConfirmPlantVarietyActivity::class.java)
+            startActivity(intent)
+        }
+
+        // 취소 버튼 클릭
+        buttonSearchPlantVarietyCancel.setOnClickListener {
+            val intent = Intent(baseContext, AddPlantActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
