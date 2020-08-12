67bc06d5d34c91c641ee9c7c4d68974bf1942faf
Dong-Young Kim <31337.persona@gmail.com>
2020-08-12 18:58:16 +0900

    feat(user): add user entity

---
+++ back-end/src/entity/User.ts
@@ -0,0 +1,34 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { UserPlant } from "./UserPlant";
+
+@Entity()
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @OneToMany((type) => UserPlant, (userPlant) => userPlant.plant)
+  userPlants: UserPlant[];
+
+  @CreateDateColumn()
+  created: Date;
+
+  @UpdateDateColumn()
+  updated: Date;
+}
