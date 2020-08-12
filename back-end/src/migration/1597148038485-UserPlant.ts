2aa8bc9076286cf9560fa5aa282571557dd5a404
Dong-Young Kim <31337.persona@gmail.com>
2020-08-12 18:58:48 +0900

    style: format with prittier

--- back-end/src/migration/1597148038485-UserPlant.ts
+++ back-end/src/migration/1597148038485-UserPlant.ts
@@ -1,14 +1,15 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class UserPlant1597148038485 implements MigrationInterface {
-    name = 'UserPlant1597148038485'
+  name = "UserPlant1597148038485";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("ALTER TABLE `user_plant` ADD `name` varchar(255) NOT NULL");
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      "ALTER TABLE `user_plant` ADD `name` varchar(255) NOT NULL"
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("ALTER TABLE `user_plant` DROP COLUMN `name`");
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query("ALTER TABLE `user_plant` DROP COLUMN `name`");
+  }
 }
