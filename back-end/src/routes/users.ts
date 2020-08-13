f8a168d3249e67aaf02c7bac38f97cdbaa4d74f5
Dong-Young Kim <31337.persona@gmail.com>
2020-08-13 12:16:41 +0900

    feat(user): encrypt data when post data

--- back-end/src/routes/users.ts
+++ back-end/src/routes/users.ts
@@ -1,14 +1,18 @@
+import * as bcrypt from "bcrypt";
 import { Request, Response, Router } from "express";
 import { User } from "../entity/User";
 
 const router = Router();
 
+const saltRounds = 10;
+
 router.post("/", async (request: Request, response: Response) => {
   const user = new User();
 
-  // user.email = request.body.email;
-  // user.username = request.body.username;
-  // user.password = request.body.password;
+  user.email = request.body.email;
+  user.username = request.body.username;
+  user.password = await bcrypt.hash(request.body.password, saltRounds);
+  await user.save();
 
   response.status(201).json(user);
 });
